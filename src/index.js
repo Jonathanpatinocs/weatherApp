7ad4c929744844dfebba5044e32a075601829e29
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-06-19 20:24:19 -0400

    api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+const API_KEY = "WVMWGU5GXHSXYGPYAQLA5BGPR";
+
+async function getWeather(location) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}` , {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .catch(function(err) {
+        return err;
+        
+    });
+    return response;
+}
+
+ function displayWeather() {
+    const location = "Mexico";
+    const weather = getWeather(location);
+    console.log(weather);
+}
+displayWeather();
